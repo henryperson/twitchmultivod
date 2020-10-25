7d4a6b2d3c2eebbf86ac18a5b9ceb819bcc84472
Henry Fisher <henry.wfisher@gmail.com>
2020-10-25 19:56:12 -0400

    Updates

---
+++ precache-manifest.1158cde99c834cde1b0b2aceac74d2d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77b827af77b7d47f9fed3401e78cc5b",
+    "url": "/twitchsync/index.html"
+  },
+  {
+    "revision": "1972857954cb3df83327",
+    "url": "/twitchsync/static/css/main.c11e2e75.chunk.css"
+  },
+  {
+    "revision": "ac00bb42aa685d57137e",
+    "url": "/twitchsync/static/js/2.b8d8e69c.chunk.js"
+  },
+  {
+    "revision": "d2ff67a5f290a85b508a595a768d7cfc",
+    "url": "/twitchsync/static/js/2.b8d8e69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1972857954cb3df83327",
+    "url": "/twitchsync/static/js/main.c829ac9e.chunk.js"
+  },
+  {
+    "revision": "6e3ac5668b197f166000",
+    "url": "/twitchsync/static/js/runtime-main.91cd0e4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twitchsync/static/media/logo.5d5d9eef.svg"
+  }
+]);
