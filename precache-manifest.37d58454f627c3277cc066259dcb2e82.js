bf9eede36f92619b37e3c702e87cc2dddc2d4589
Henry Fisher <henry.wfisher@gmail.com>
2020-10-25 20:57:18 -0400

    Updates

---
+++ precache-manifest.37d58454f627c3277cc066259dcb2e82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2e4a1db33f0de49922806e2325f657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67734129f11ee4969228",
+    "url": "/static/css/main.c11e2e75.chunk.css"
+  },
+  {
+    "revision": "ac00bb42aa685d57137e",
+    "url": "/static/js/2.b8d8e69c.chunk.js"
+  },
+  {
+    "revision": "d2ff67a5f290a85b508a595a768d7cfc",
+    "url": "/static/js/2.b8d8e69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67734129f11ee4969228",
+    "url": "/static/js/main.9f6048f6.chunk.js"
+  },
+  {
+    "revision": "bfb7bac18c6ddfc95bd7",
+    "url": "/static/js/runtime-main.bf2b001b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/twitchsync/precache-manifest.339d1a41ab52581a87d9a7605862df69.js"
+  "/precache-manifest.37d58454f627c3277cc066259dcb2e82.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/twitchsync/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
